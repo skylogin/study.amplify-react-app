57b5bee91b7b20f800186178402a4da90afda31f
32791 <ch.jung@kbs.co.kr>
2021-07-31 15:27:38 +0900

    공통컴포넌트 추가

---
+++ basic-authentication/src/components/common/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Button({ onClick, title }) {
+    return (
+        <button style={StyleSheet.button} onClick={onCLick}>
+            {title}
+        </button>
+    )
+}
+
+const styles = {
+    button: {
+        backgroundColor: '#006b7c',
+        color: 'white',
+        width: 316,
+        height: 45,
+        fontWeight: '600',
+        fontSize: 14,
+        cursor: 'pointer',
+        border: 'none',
+        outline: 'none',
+        borderRadius: 3,
+        marginTop: '25px',
+        boxShadow: '0px 1px 3px rgba(0, 0, 0, .3)',
+    }
+}
