1cc225b3b9cdacc02b547768c184452b9b041197
32791 <ch.jung@kbs.co.kr>
2021-08-01 23:47:43 +0900

    aws 관련 backend 추가

--- ecommerceapp/amplify/backend/function/ecommercefunction/src/app.js
+++ ecommerceapp/amplify/backend/function/ecommercefunction/src/app.js
@@ -16,6 +16,19 @@ See the License for the specific language governing permissions and limitations 
 	STORAGE_PRODUCTTABLE_STREAMARN
 Amplify Params - DO NOT EDIT */
 
+const AWS = require('aws-sdk');
+const { v4: uuid } = require('uuid');
+
+const cognito = new AWS.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18'
+});
+
+var userpoolId = process.env.ECOMMERCEAPPD11C0EB8;
+
+const region = process.env.REGION;
+const ddb_table_name = process.env.STORAGE_PRODUCTTABLE_NAME;
+const docClient = new AWS.DynamoDB.DocumentClient({ region });
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
@@ -33,13 +46,70 @@ app.use(function(req, res, next) {
 });
 
 
+
+
+async function getGroupsForUser(event){
+  let userSub = event.requestContext
+    .identity
+    .cognitoAuthenticationProvider
+    .split(':CognitoSignIn:')[1];
+  
+    let userParams = {
+      UserPoolId: userpoolId,
+      Filter: `sub = "${userSub}"`,
+    };
+
+    let userData = await cognito.listUsers(userParams).promise();
+    const user = userData.Users[0];
+    var groupParams = {
+      UserPoolId: userpoolId,
+      Username: user.Username
+    };
+    const groupData = await cognito.adminListGroupsForUser(groupParams).promise();
+    return groupData;
+}
+
+
+async function canPerformAction(event, group){
+  return new Promise(async (resolve, reject) => {
+    if(!event.requestContext.identity.cognitoAuthenticationProvider){
+      return reject();
+    }
+    const groupData = await getGroupsForUser(event);
+    const groupsForUser = groupData.Groups.map(group => group.GroupName);
+    if(groupsForUser.includes(group)){
+      resolve();
+    } else{
+      reject('user not in group, cannot perform action..');
+    }
+  });
+}
+
+
+async function getItems(){
+  var params = { TableName: ddb_table_name };
+  try{
+    const data = await docClient.scan(params).promise();
+    return data;
+  } catch(err){
+    return err;
+  }
+}
+
+
+
 /**********************
  * Example get method *
  **********************/
 
 app.get('/products', function(req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  try{
+    const data = await getItems();
+    res.json({ data: data });
+  } catch(err){
+    res.json({ error: err });
+  }
 });
 
 app.get('/products/*', function(req, res) {
@@ -53,7 +123,21 @@ app.get('/products/*', function(req, res) {
 
 app.post('/products', function(req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  const { body } = req;
+  const { event } = req.apiGateway;
+
+  try{
+    await canPerformAction(event, 'Admin');
+    const input = { ...body, id: uuid() };
+    var params = {
+      TableName: ddb_table_name_name,
+      Item: input
+    }
+    await docClient.put(params).promise();
+    res.json({ success: 'item saved to database..' });
+  } catch(err){
+    res.json({ error: err });
+  }
 });
 
 app.post('/products/*', function(req, res) {
@@ -81,7 +165,18 @@ app.put('/products/*', function(req, res) {
 
 app.delete('/products', function(req, res) {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  const { event } = req.apiGateway;
+  try{
+    await canPerformAction(event, 'Admin');
+    var params = {
+      TableName: ddb_table_name_name,
+      Key: { id: req.body.id }
+    }
+    await docClient.delete(params).promise();
+    res.json({ success: 'successfully deleted item..' });
+  } catch(err){
+    res.json({ error: err });
+  }
 });
 
 app.delete('/products/*', function(req, res) {
--- ecommerceapp/src/index.js
+++ ecommerceapp/src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Router from './components/Router';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'antd/dist/antd.css';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Router />, document.getElementById('root'));
