53e031ccd3d09b1bc515551a01883f592ed91675
32791 <ch.jung@kbs.co.kr>
2021-08-01 23:13:50 +0900

    람다를 이용해 사진잘라내기 추가

--- lambda-trigger-example/src/App.js
+++ lambda-trigger-example/src/App.js
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react';
-import { Auth } from 'aws-amplify';
+import { Auth, Storage } from 'aws-amplify';
+import { v4 as uuid } from 'uuid';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import './App.css';
 
 function App() {
   const [user, updateUser] = useState(null);
+  const [images, setImages] = useState([]);
+
   useEffect(() => {
     Auth.currentAuthenticatedUser()
       .then(user => updateUser(user))
       .catch(err => console.log(err));
+
+    fetchImages();
   }, []);
+
+
+  async function onChange(e){
+    const file = e.target.files[0];
+    const filetype = file.name.split('.')[file.name.split.length -1];
+    await Storage.put(`${uuid()}.${filetype}`, file);
+    fetchImages();
+  }
+  async function fetchImages(){
+    const files = await Storage.list('');
+    const signedFiles = await Promise.all(files.map(async file => {
+      const signedFile = await Storage.get(file.key);
+      return signedFile;
+    }));
+    setImages(signedFiles);
+  }
+
+
 
   let isAdmin = false;
   if(user){
@@ -23,9 +46,16 @@ function App() {
 
   return (
     <div className="App">
-      <header>
+      <header className="App-header">
         <h1>Hello World</h1>
         { isAdmin && <p>Welcome, Admin</p> }
+
+        <input type="file" onChange={onChange} />
+        {
+          images.map(image => (
+            <img src={image} key={image} style={{ width: 500 }} />
+          ))
+        }
       </header>
       <AmplifySignOut />
     </div>
